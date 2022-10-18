c2dcab56441915eaaa2af0ef1f63a8e96e2713e6
zhan1.ba0999@gmail.com <zhan1.ba0999@gmail.com>
2022-10-18 20:27:22 +0700

    添加alter/toast/dialog/var相关公共部分

--- src/js/base.js
+++ src/js/base.js
@@ -42,3 +42,142 @@ function resetBodyFontSize() {
 }
 
 resetBodyFontSize();
+
+// alter弹出框
+var Alter = {
+	show: function (text, callback) {
+		var alterDocument = document.createElement('div');
+		alterDocument.setAttribute('class', 'alter');
+		var alterInnerHtml = `
+			<div class="alter_background"></div>
+			<div class="alter_box">
+				<div class="alter_title">提示</div>
+				<div class="alter_text">`+ text + `</div>
+				<div class="public_btn alter_btn">
+					<span class="alter_btn_text">确认</span>
+					<div class="alter_btn_loading"></div>
+				</div>
+			</div>
+		`;
+		$(alterDocument).html(alterInnerHtml);
+		$("body").append(alterDocument);
+		$(".alter_box").animate({ 'opacity': 1 }, 200);
+
+		$('.alter_background').on('click', function () {
+			$('.alter_box').animate({ 'opacity': '0' }, 200);
+			setTimeout(function () {
+				$('.alter').remove();
+			}, 200);
+		});
+		$(".alter_btn").on("click", function () {
+			$(".alter_btn_text").hide()
+			$(".alter_btn_loading").fadeIn();
+			if (callback) {
+				callback();
+			}
+		});
+	},
+	hide: function () {
+		$('.alter_box').animate({ 'opacity': '0' }, 200);
+		setTimeout(function () {
+			$('.alter').remove();
+		}, 200);
+	}
+}
+
+// 轻提示 toast
+var Toast = {
+	show: function (text) {
+		var toastDocument = document.createElement('div');
+		toastDocument.setAttribute('class', 'toast');
+		var toastInnerHtml = `
+			<div class="toast_box">` + text + `</div>
+		`;
+		$(toastDocument).html(toastInnerHtml);
+		$("body").append(toastDocument);
+		$(".toast").animate({ 'top': 0 }, 200);
+		setTimeout(function () {
+			Toast.hide();
+		}, 2000);
+	},
+	hide: function () {
+		$('.toast').animate({ 'top': '-60px' }, 200);
+		setTimeout(function () {
+			$('.toast').remove();
+		}, 200);
+	}
+}
+
+// dialog 弹出框
+var AddDialog = {
+	show: function (callback) {
+		var addDialogDocument = document.createElement('div');
+		addDialogDocument.setAttribute('class', 'dialog');
+		var addDialogInnerHtml = `
+			<div class="dialog_background"></div>
+			<div class="dialog_box">
+				<div class="dialog_title">新增</div>
+				<div class="dialog_input_box">
+          <input class="public_input add_name" type="text" name="account" placeholder="请输入域名" />
+        </div>
+				<p class="dialog_prompt add_name_prompt"></p>
+				<div class="dialog_btn_box" value="2">
+          <button class="public_btn warning add_status" type="button" value="1">
+            正常
+          </button>
+          <button class="public_btn add_status" type="button" value="2">
+            未运行
+          </button>
+          <button class="public_btn warning add_status" type="button" value="3">
+            已禁用
+          </button>
+        </div>
+        <p class="dialog_prompt add_status_prompt"></p>
+				<div id="dialog_submit" class="public_btn">
+					<span class="dialog_btn_text">确认</span>
+					<div class="dialog_btn_loading"></div>
+				</div>
+			</div>
+		`;
+		$(addDialogDocument).html(addDialogInnerHtml);
+		$("body").append(addDialogDocument);
+		$(".dialog_box").animate({ 'opacity': 1 }, 200);
+		$('.dialog_background').on('click', function () {
+			$('.dialog_box').animate({ 'opacity': '0' }, 200);
+			setTimeout(function () {
+				$('.dialog').remove();
+			}, 200);
+		});
+		$(".add_status").on("click", function () {
+			var _self = $(this);
+			var _value = $(this).attr('value');
+			_self.removeClass('warning');
+			_self.siblings('.public_btn').addClass('warning');
+			_self.parent('.dialog_btn_box').attr("value", _value);
+		});
+		$("#dialog_submit").on("click", function () {
+			var _name = $(".add_name").val();
+			var _status = $(".dialog_btn_box").attr('value');
+			var _add_name_prompt = $(".add_name_prompt");
+			if (!_name) {
+				_add_name_prompt.text('域名不能为空');
+				return;
+			}
+			$(".dialog_btn_text").hide()
+			$(".dialog_btn_loading").fadeIn();
+			var _data = {
+				url: _name,
+				status: _status,
+			};
+			if (callback) {
+				callback(_data);
+			};
+		});
+	},
+	hide: function () {
+		$('.dialog_box').animate({ 'opacity': '0' }, 200);
+		setTimeout(function () {
+			$('.dialog').remove();
+		}, 200);
+	}
+}
